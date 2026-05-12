98b837534e4ca9c3de29911a8f282aace1d2ad2d
Abiola Makinde <abmknd@gmail.com>
2026-05-12 04:53:30 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/lethe/' : '/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
